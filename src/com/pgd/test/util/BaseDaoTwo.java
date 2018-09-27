ca61262486e7bffd48fd35cf9f5c77ebcaa6eeed
zhao <zzg19910406@163.com>
2018-09-27 19:36:21 +0800

    第一次提交

---
+++ src/com/pgd/test/util/BaseDaoTwo.java
@@ -0,0 +1,15 @@
+/*package com.pgd.test.util;
+
+import javax.annotation.Resource;
+
+import org.hibernate.SessionFactory;
+import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
+
+public class BaseDaoTwo extends HibernateDaoSupport {
+	// 对应数据spmhbe2
+	@Resource(name = "SessionFactory2")
+	public void setSession(SessionFactory sessionFactory) {
+		this.setSessionFactory(sessionFactory);
+	}
+}
+*/
